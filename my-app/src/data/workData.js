1c53e6eae73c7839d39b3a697fdc17c609c3763b
louie camacho dxa <louiecamacho@gmail.com>
2022-02-28 09:35:54 -0600

    added external data, getting faster at it

--- my-app/src/data/workData.js
+++ my-app/src/data/workData.js
@@ -1,20 +1,30 @@
 export default [
     {
-        position: "Front End Developer",
-        name: "Ogilvy & Mather",
-        year: "2015 - Present",
-        description: "Developed, managed content and maintained HTML5 ads using GSAP Javascript library. Built responsive e-mail templates, created animated videos for Social Ads. Assisted in design and layout development. Trained the dev team in all aspects of development, including Git."
+        position: 'Front End Developer',
+        name: 'Depaul University',
+        year: '2021 - present',
+        description:
+            'Building reusable, scalable, and modular CSS to use on pages and elements with Bootstrap. Taking existing HTML/CSS and refactoring into cleaner, optimized code without changing external behavior.',
     },
     {
-        position: "Front End Developer",
-        name: "Staples/quill",
-        year: "2014 - 2015",
-        description: "Designed, developed and maintained Wordpress pages. Created static banners for use on quill.com. Built a responsive email template used for all future email campaigns."
+        position: 'Front End Developer',
+        name: 'Ogilvy & Mather',
+        year: '2015 - 2021',
+        description:
+            'Developed, managed content and maintained HTML5 ads using GSAP Javascript library. Built responsive e-mail templates, created animated videos for Social Ads. Assisted in design and layout development. Trained the dev team in all aspects of development, including Git.',
     },
     {
-        position: "Web Designer & Developer",
-        name: "Media Made Great",
-        year: "2010 - 2014",
-        description: "Created & maintained the company’s website. Client work included websites, landing pages, and email marketing campaigns. Primarily hand coded HTML5, CSS3 and Javascript. Other responsibilities include production work and layout design."
+        position: 'Front End Developer',
+        name: 'Staples/quill',
+        year: '2014 - 2015',
+        description:
+            'Designed, developed and maintained Wordpress pages. Created static banners for use on quill.com. Built a responsive email template used for all future email campaigns.',
     },
-]
+    {
+        position: 'Web Designer & Developer',
+        name: 'Media Made Great',
+        year: '2010 - 2014',
+        description:
+            'Created & maintained the company’s website. Client work included websites, landing pages, and email marketing campaigns. Primarily hand coded HTML5, CSS3 and Javascript. Other responsibilities include production work and layout design.',
+    },
+];
